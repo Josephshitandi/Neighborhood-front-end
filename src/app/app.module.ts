b4ddf648eb9943ffaabe87c3b96cfbb2516a7563
Josephshitandi <jsphshtnd@gmail.com>
2020-12-04 12:11:46 +0300

    Resolved style error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';;
-import { ContactComponent } from './contact/contact.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    NavigationComponent,
     
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
