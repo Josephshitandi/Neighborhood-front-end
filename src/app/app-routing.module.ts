e124ad09d9d93b60292f4c8aa3f4eaf7ea77ba3e
rowenarono95 <rowenarono@gmail.com>
2020-12-06 12:25:18 +0300

    added background image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from '../app/about/about.component'
+import {BusinessComponent} from '../app/business/business.component'
  
 const routes: Routes = [
 {path: 'about', component: AboutComponent },
+{path: 'business', component: BusinessComponent },
 ];
 
 @NgModule({
