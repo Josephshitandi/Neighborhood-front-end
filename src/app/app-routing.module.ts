4ef8a4ab95859bd27b4e8c008c5487d9a25efa8c
rowenarono95 <rowenarono@gmail.com>
2020-12-06 13:48:36 +0300

    added corousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AboutComponent} from '../app/about/about.component'
-import {BusinessComponent} from '../app/business/business.component'
+
  
 const routes: Routes = [
-{path: 'about', component: AboutComponent },
-{path: 'business', component: BusinessComponent },
 ];
 
 @NgModule({
