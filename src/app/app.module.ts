4ab84dfc2b0717f542415dfdc3942f4c426bdc0c
Daniel-darnell <darnelldanny1997@gmail.com>
2020-12-07 03:25:28 +0300

    sign-up done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { UserService } from './shared/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from '../app/user/sign-in/sign-in.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-  
+    SignUpComponent,
+    HomeComponent,
+    UserComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
