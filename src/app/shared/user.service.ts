cf010ecb377a1bb9584adc5091463a427ef56bfa
Daniel-darnell <darnelldanny1997@gmail.com>
2020-12-07 08:32:19 +0300

    Added auth sign in and routes

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {  Response } from "@angular/http";
-import {Observable} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Response } from "@angular/http";
+import { Observable } from 'rxjs';
 //import 'rxjs/add/operator/map';
 import { User } from './user.model';
 
@@ -10,7 +10,7 @@ export class UserService {
   readonly rootUrl = 'http://localhost:35257';
   constructor(private http: HttpClient) { }
 
-  registerUser(user : User){
+  registerUser(user: User) {
     const body: User = {
       UserName: user.UserName,
       Password: user.Password,
@@ -18,7 +18,18 @@ export class UserService {
       FirstName: user.FirstName,
       LastName: user.LastName
     }
-    return this.http.post(this.rootUrl + '/api/User/Register', body);
+    var reqHeader = new HttpHeaders({'No-Auth':'True'});
+    return this.http.post(this.rootUrl + '/api/User/Register', body,{headers : reqHeader});
+  }
+
+  userAuthentication(userName, password) {
+    var data = "username=" + userName + "&password=" + password + "&grant_type=password";
+    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True' });
+    return this.http.post(this.rootUrl + '/token', data, { headers: reqHeader });
+  }
+
+  getUserClaims(){
+   return  this.http.get(this.rootUrl+'/api/GetUserClaims');
   }
 
 }
